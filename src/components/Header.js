05bcc4f2f0ed65272887869b83422bd982670063
Valentina Karpova <bfeatb@mail.ru>
2021-10-26 11:26:52 +0100

    style: code formatting

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import logo from '../images/logo.svg';
+import React from "react";
+import logo from "../images/logo.svg";
 
 function Header() {
-    return (
-        <header className="header">
-            <a href="#"><img className="header__logo" src={logo} alt="логотип" /></a>
-        </header>
-    );
+  return (
+    <header className="header">
+      <a href="#">
+        <img className="header__logo" src={logo} alt="логотип" />
+      </a>
+    </header>
+  );
 }
 
 export default Header;
