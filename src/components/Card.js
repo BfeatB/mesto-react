c7af4205059945bc1a9e3dcb2d14a8dc981f2ff0
Valentina Karpova <bfeatb@mail.ru>
2021-11-07 18:03:37 +0000

    feat: add owner likes and delete button

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
+import CurrentUserContext from "../context/CurrentUserContext";
 
 function Card({ card, onCardClick }) {
+  const currentUser = useContext(CurrentUserContext);
+  const isOwn = card.owner._id === currentUser._id;
+  const cardDeleteButtonClassName = `card__delete-button button ${
+    isOwn ? "card__delete-button_visible" : ""
+  }`;
+
+  const isLiked = card.likes.some((i) => i._id === currentUser._id);
+  const cardLikeButtonClassName = `card__like-button button ${
+    isLiked ? "card__like-button_active" : ""
+  }`;
+
   return (
     <li className="card">
-      <button type="button" className="card__delete-button button"></button>
+      <button type="button" className={cardDeleteButtonClassName}></button>
       <img
         className="card__img"
         src={card.link}
@@ -14,7 +26,7 @@ function Card({ card, onCardClick }) {
       />
       <div className="card__items">
         <h2 className="card__capture">{card.name}</h2>
-        <button type="button" className="card__like-button button"></button>
+        <button type="button" className={cardLikeButtonClassName}></button>
         <p className="card__like-counter">{card.likes.length}</p>
       </div>
     </li>
